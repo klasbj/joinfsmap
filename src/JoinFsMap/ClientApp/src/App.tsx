15a0606ab632fb0e78164bb0d6a79cf640659352
Klas Björkqvist <klas.bjorkqvist@gmail.com>
2019-02-10 14:49:08 +0100

    Get and display some data from the server.

--- src/JoinFsMap/ClientApp/src/App.tsx
+++ src/JoinFsMap/ClientApp/src/App.tsx
@@ -1,28 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Fetcher } from './Fetcher';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+interface Status {
+  timeStamp: string,
+  atcs: Atc[],
+  pilots: Pilot[]
 }
 
-export default App;
+interface Position {
+  latitude: number,
+  longitude: number,
+  altitude: number
+}
+
+interface Entity {
+  callsign: string,
+  userName: string,
+  position: Position,
+  key: string
+}
+
+interface Atc extends Entity {
+  frequency: number[]
+}
+
+interface Pilot extends Entity {
+  groundSpeed: number,
+  heading: number,
+  aircraftType: string,
+  aircraftTypeShort: string,
+  trail: { timeStamp: string, position: Position }
+}
+
+export function App() {
+  return (
+    <div className="App">
+      <Fetcher<Status> src="api/ServerStatus">
+        {(data, loading, error) => {
+          if (loading) {
+            return <p>Loading...</p>
+          }
+
+          if (error !== null || data === null) {
+            return <p>Error: {error}</p>
+          }
+
+
+          return (<div>
+            <p>Status at {data.timeStamp}</p>
+            <h3>ATC</h3>
+            <ul>
+              {data.atcs.map(a => <li key={a.key}>{a.callsign} {a.frequency} &mdash; {a.userName}</li>)}
+            </ul>
+            <h3>Pilots</h3>
+            <ul>
+              {data.pilots.map(a => <li key={a.key}>{a.callsign} {a.userName} {a.aircraftType}</li>)}
+            </ul>
+          </div>);
+        }}
+      </Fetcher>
+    </div>
+  );
+}
